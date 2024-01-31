f9de1bc01406c17e64cedd4433046d58555d2d66
Helmut Martinez <helmutmartinez128@gmail.com>
2024-01-31 11:27:43 -0400

    Mejora de codigo

--- src/app/app/header/header.component.ts
+++ src/app/app/header/header.component.ts
@@ -13,20 +13,33 @@ import { NgIf, NgFor } from '@angular/common';
 })
 export class HeaderComponent {
   codigosIngresados: string[] = [];
-  
+  empleadosIngresados: any[] = []; // Array para almacenar la información de los empleados ingresados
   codigoIngresado: string = '';
   date = new Date();
   empleado: any;
   empleadoEncontrado: boolean = false;
+  codigoInvalido: boolean = false;
 
   constructor(private empleadoService: EmpleadosService) {}
 
   showEmpleadosDetails(code: string){
     this.empleado = this.empleadoService.getEmployeeByCode(code);
     this.empleadoEncontrado = !!this.empleado; 
+
+    if (this.empleadoEncontrado) {
      // Agregar el código ingresado al array
-  this.codigosIngresados.push(code);
+    this.codigosIngresados.push(code);
+    // Agregar la información del empleado al array de empleados ingresados
+    this.empleadosIngresados.push(this.empleado);
+    this.empleadosIngresados.reverse();
+     // Restablecer el estado del mensaje de error
+     this.codigoInvalido = false;
+   }
+   else {
+    // Marcar como código inválido y mostrar el mensaje de error
+    this.codigoInvalido = true;
   }
+}
 
   onKeyPress(event: KeyboardEvent) {
     const inputElement = event.target as HTMLInputElement;
--- src/app/services/empleados.service.ts
+++ src/app/services/empleados.service.ts
@@ -10,11 +10,15 @@ export class EmpleadosService {
     { code: '0000', nombreCompleto: 'Julian Alvarez', turno: 'Noche', entryTime: '09:02', exitTime: '18:00' },
     { code: '0001', nombreCompleto: 'Helmut Ibrahim Martinez Aragón', turno: 'Noche', entryTime: '07:10', exitTime: '18:00' },
     { code: '0002', nombreCompleto: 'Ibrahim Helmut Martinez Aragon ', turno: 'Dia', entryTime: '08:30', exitTime: '17:30' },
-    { code: '0003', nombreCompleto: 'Shakira Martinez Aragon ', turno: 'Dia',entryTime: '08:10', exitTime: '17:30' },
+    { code: '0003', nombreCompleto: 'Shakira Martinez Aragon ', turno: 'Dia', entryTime: '08:10', exitTime: '17:30' },
     { code: '0004', nombreCompleto: 'Pedro Infante', turno: 'Noche', entryTime: '09:23', exitTime: '18:00' },
-    { code: '0004', nombreCompleto: 'Pepe del rosario Cruz Moreno', turno: 'Noche', entryTime: '09:03', exitTime: '18:00' },
-  
-  ];
+    { code: '0005', nombreCompleto: 'Pepe del rosario Cruz Moreno', turno: 'Noche', entryTime: '09:03', exitTime: '18:00' },
+    { code: '0006', nombreCompleto: 'María Fernández', turno: 'Noche', entryTime: '08:45', exitTime: '18:00' },
+    { code: '0007', nombreCompleto: 'Juan Pérez', turno: 'Día', entryTime: '08:00', exitTime: '17:30' },
+    { code: '0008', nombreCompleto: 'Ana González', turno: 'Día', entryTime: '07:30', exitTime: '17:00' },
+    { code: '0009', nombreCompleto: 'Luis Martínez', turno: 'Noche', entryTime: '09:15', exitTime: '18:00' }
+  ]
+  ;
 
   constructor() { }
 
